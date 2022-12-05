b51e1691592a97ea0802a35a69115a5d7a6d95a3
parkjongjoon7128 <whdwnsdk8111@naver.com>
2022-12-05 15:42:46 +0900

    openweathermap api 실시간으로 데이터 받아오기 - 성공

--- screens/WeatherScreen.js
+++ screens/WeatherScreen.js
@@ -16,8 +16,14 @@ const WeatherScreen = () => {
   });
 
   useEffect(() => {
-    setTimeout(() => getWeatherApi(), 10000);
+    if (weather.condition.length) {
+      setTimeout(() => getWeatherApi(), 10000);
+    }
   }, [weather]);
+
+  useEffect(() => {
+    getWeatherApi();
+  }, []);
 
   const getWeatherApi = async () => {
     const API_KEY = '2cd518b6f461fdf5a043511d0292ab5c';
