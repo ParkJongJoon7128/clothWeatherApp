d51831a4afb24826294741d74519d8498c59df1e
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-01-11 01:10:24 +0900

    KeyboardAvoidingView 추가

--- components/TextInput.js
+++ components/TextInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {KeyboardAvoidingView} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
 
 const TextInputs = ({style, setText, value}) => {
@@ -7,15 +8,19 @@ const TextInputs = ({style, setText, value}) => {
   };
 
   return (
-    <TextInput
-      style={style}
-      // inlineImageLeft="search_icon"
-      numberOfLines={1}
-      placeholderTextColor="gray"
-      textAlign="left"
-      onChangeText={ChangeText}
-      value={value}
-    />
+    <KeyboardAvoidingView
+      behavior={Platform.select({ios: 'padding', android: undefined})}
+    >
+      <TextInput
+        style={style}
+        // inlineImageLeft="search_icon"
+        numberOfLines={1}
+        placeholderTextColor="gray"
+        textAlign="left"
+        onChangeText={ChangeText}
+        value={value}
+      />
+    </KeyboardAvoidingView>
   );
 };
 
